6f59b6a4236b95443d0fcb598f6257ef3e2968a6
Paul Borawski <paulborawski14@gmail.com>
2016-04-04 18:28:03 -0700

    put in your zip code

--- public/weather/app.js
+++ public/weather/app.js
@@ -1,10 +1,68 @@
-APPID = "aa098a1d6355e525873e968085163eae";
+APPID = "87477587e12826b182b4b7cef844ec61";
 var temp;
 var loc;
 var icon;
 var humidity;
 var wind;
 var direction;
+
+function updateByZip(zip) {
+  var url = "http://api.openweathermap.org/data/2.5/weather?" +
+    "zip=" + zip +
+    "&APPID=" + APPID;
+  sendRequest(url);
+}
+
+function updateByGeo(lat, lon) {
+  var url = "http://api.openweathermap.org/data/2.5/weather?" +
+  "lat=" + lat +
+  "&lon=" + lon +
+  "&APPID" + APPID;
+  sendRequest(url);
+}
+
+function sendRequest(url) {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      var data = JSON.parse(xmlhttp.responseText);
+      var weather = {};
+      weather.icon = data.weather[0].id;
+      weather.humidity = data.main.humidity;
+      weather.wind = data.wind.speed;
+      weather.direction= degreesToDirection(data.wind.deg);
+      weather.loc = data.name;
+      weather.temp = K2F(data.main.temp);
+      update(weather);
+    }
+  };
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+}
+
+function degreesToDirection(degrees) {
+  var range = 360/8;
+  var low = 360 - range/2;
+  var high = (low + range) % 360;
+  var angles = [ "N", "NE", "E", "SE", "S", "SW", "W", "NW" ];
+  for ( i in angles) {
+    if( degrees >= low && degrees < high) {
+      return angles[i];
+    }
+
+    low = (low + range) % 360;
+    high = (high + range) % 360;
+  }
+  return "N";
+}
+
+function K2C(k) {
+  return Math.round(k - 273.15);
+}
+
+function K2F(k) {
+  return Math.round(k*(9/5)-459.67);
+}
 
 function update(weather) {
   wind.innerHTML = weather.wind;
@@ -15,6 +73,10 @@ function update(weather) {
   icon.src = "imgs/codes/" + weather.icon + ".png";
 }
 
+function showPosition(position) {
+  updateByGeo(position.coords.latitude, position.coords.longitude);
+}
+
 window.onload = function () {
   temp = document.getElementById("temperature");
   loc = document.getElementById("location");
@@ -23,13 +85,10 @@ window.onload = function () {
   wind = document.getElementById("wind");
   direction = document.getElementById("direction");
 
-  var weather = {};
-  weather.wind = 3.5;
-  weather.direction = "N";
-  weather.humidity = 35;
-  weather.loc = "Boston";
-  weather.temp = "45";
-  weather.icon = 200;
-
-  update(weather);
+  if (!navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+   var zip = window.prompt("Could not discover your location: What is your zip code?");
+   updateByZip(zip);
+ }
 }
---
+++ public/weather/readme.markdown
@@ -0,0 +1,3 @@
+#API KEY
+
+The api key si not working so for now you will have to enter location by zip code.
